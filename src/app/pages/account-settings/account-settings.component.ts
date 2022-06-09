2c3db378bbd285a1fdb78db1b5d24b2555181f14
irvinzato <multizato@hotmail.com>
2022-06-08 21:51:24 -0500

    Recorrido a elementos nodos para comparar atributos y añadir clase bajo condicion

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
   
   linkTheme = document.querySelector('#theme'); //Id en index.html
+  links!: NodeListOf<Element>;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.links = document.querySelectorAll('.selector');
   }
 
   changeTheme( theme: string ) {
@@ -19,6 +21,21 @@ export class AccountSettingsComponent implements OnInit {
     const url = `./assets/css/colors/${theme}.css`;
     this.linkTheme?.setAttribute( 'href', url );
     localStorage.setItem( 'theme', url );
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    
+    this.links.forEach( element => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if( btnThemeUrl === currentTheme ) {
+        element.classList.add('working');
+      }
+    });
   }
 
 }
