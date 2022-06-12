b361336903d1b6c4ba7e3d6472f867021814ed7b
irvinzato <multizato@hotmail.com>
2022-06-12 00:47:16 -0500

    Primer Observable para entender sus conceptos basicos

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    const obs$ = new Observable( observer => {
+      let i = -1;
+      const intervalo = setInterval( () => {
+        i++;
+        observer.next(i);
+
+        if( i === 4 ) {
+          clearInterval( intervalo );
+          observer.complete();
+        }
+        if( i === 2 ) {
+          observer.error('i llego al valor de 2 que tome como error');
+        }
+      }, 1000);
+    });
+
+    obs$.subscribe( 
+    valor => console.log('Subs: ', valor),
+    err => console.warn('Error: ', err),
+    () => console.info('Observable terminado')
+    );
+  }
 
   ngOnInit(): void {
   }
