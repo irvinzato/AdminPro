550ab2f71934392aeaa85ca2adb1bb55eb5c1a16
irvinzato <multizato@hotmail.com>
2022-06-13 22:36:38 -0500

    Uso de unsubscribe para Observables, para parar sus emiciones

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,7 +7,9 @@ import { retry, take, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnDestroy {
+
+  intervalSubs: Subscription;
 
   constructor() { 
     /* this.retornaObervable().pipe(
@@ -17,16 +19,18 @@ export class RxjsComponent implements OnInit {
     err => console.warn('Error: ', err),
     () => console.info('Observable terminado')
     ); */
-    this.retornaIntervalo().subscribe( (valor) => console.log("Valor es ", valor) )
+    this.intervalSubs = this.retornaIntervalo().subscribe( (valor) => console.log("Valor es ", valor) );
+  
+  }
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
   }
 
-  ngOnInit(): void {
-  }
 
   retornaIntervalo(): Observable<number> {
     return interval(500)
           .pipe(
-            take(10),   //take dice cuantas emisiones del Observable necesito
+            //take(10),   //take dice cuantas emisiones del Observable necesito
             map( valor => { //map transforma la informacion que resibe el Observable y mutarla a lo que necesito
               return valor + 1;
             }),
