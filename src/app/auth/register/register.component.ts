f64455f5b98f6121f6887210948fabf5032fffaf
irvinzato <multizato@hotmail.com>
2022-07-01 16:10:53 -0500

    Hice validaciones para mi formulario de registro, queda pendiente validar contraseñas

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,12 +8,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
+  formSubmitted = false;
+
   registerForm = this.fb.group({
     nombre: [ 'Irving', [ Validators.required, Validators.minLength(3) ] ],
-    email: [ 'irvingPrueba@gmail.com', [ Validators.required] ],
-    password: [ '123456', [ Validators.required] ],
-    password2: [ '123456', [ Validators.required] ],
-    terminos: [ false , [ Validators.required] ]
+    email: [ 'irving@prueba.com', [ Validators.required, Validators.email ] ],
+    password: [ '123456', [ Validators.required ] ],
+    password2: [ '123456', [ Validators.required ] ],
+    terminos: [ false , [ Validators.required, Validators.requiredTrue ] ]
   });
 
   constructor( private fb: FormBuilder ) { }
@@ -22,7 +24,27 @@ export class RegisterComponent implements OnInit {
   }
 
   crearUsuario() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    if( this.registerForm.valid ) {
+      console.log('Posteando formulario');
+    } else {
+      console.log('El formulario no es correcto');
+    }
+
+  }
+
+  campoNoValido( campo: string ): boolean {
+    if( this.registerForm.get(campo)?.invalid && this.formSubmitted ) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  aceptarTerminos() {
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 
 }
