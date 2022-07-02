d053a8495fa67f93c6527667b8e60030bb3b1005
irvinzato <multizato@hotmail.com>
2022-07-01 23:17:16 -0500

    Valide que las contraseñas sean iguales, caso contrario no dejo mandar el formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +14,10 @@ export class RegisterComponent implements OnInit {
     nombre: [ 'Irving', [ Validators.required, Validators.minLength(3) ] ],
     email: [ 'irving@prueba.com', [ Validators.required, Validators.email ] ],
     password: [ '123456', [ Validators.required ] ],
-    password2: [ '123456', [ Validators.required ] ],
+    password2: [ '1234567', [ Validators.required ] ],
     terminos: [ false , [ Validators.required, Validators.requiredTrue ] ]
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
   });
 
   constructor( private fb: FormBuilder ) { }
@@ -26,6 +28,7 @@ export class RegisterComponent implements OnInit {
   crearUsuario() {
     this.formSubmitted = true;
     console.log(this.registerForm.value);
+    //console.log(this.registerForm);
 
     if( this.registerForm.valid ) {
       console.log('Posteando formulario');
@@ -43,8 +46,33 @@ export class RegisterComponent implements OnInit {
     }
   }
 
+  validarContrasenas(): boolean {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if( (pass1 !== pass2)  && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  passwordsIguales(pass1: string, pass2: string) {
+    return ( formGroup: FormGroup ) => {
+      const pass1Control = formGroup.get(pass1);
+      const pass2Control = formGroup.get(pass2);
+      //Si el "setErrors" de cualquier valor del formulario trae alguna variable diferente de "null", siempre sera invalido el formulario
+      if( pass1Control?.value === pass2Control?.value ) {
+        pass2Control?.setErrors(null);
+      } else {
+        pass2Control?.setErrors({ noEsIgual: false });
+      }
+    }
+  }
+
   aceptarTerminos() {
     return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
+
 
 }
