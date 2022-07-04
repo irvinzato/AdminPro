c400373950ede53d49b3398728ce276d49ceebbc
irvinzato <multizato@hotmail.com>
2022-07-04 12:52:25 -0500

    Guarde en localStorage el token para login y registro

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 import { environment } from './../../environments/environment';
 import { RegisterForm } from '../interfaces/register-form.interface';
@@ -16,11 +17,21 @@ export class UsuarioService {
   constructor( private http: HttpClient ) { }
 
   crearUsuario( formData: RegisterForm ) {
-    return this.http.post( `${ baseUrl }/usuarios`, formData );
+    return this.http.post( `${ baseUrl }/usuarios`, formData )
+            .pipe(
+              tap( (res: any) => {
+                localStorage.setItem('token', res.token);
+              })
+            );
   }
 
   loginUsuario( formData: LoginForm ) {
-    return this.http.post( `${ baseUrl }/login`, formData );
+    return this.http.post( `${ baseUrl }/login`, formData )
+            .pipe(
+              tap( (res: any) => {
+                localStorage.setItem('token', res.token);
+              })
+            );
   }
 
 }
