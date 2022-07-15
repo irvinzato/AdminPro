2b322d9363ec6ae26f70dccec4ff9ce68357e2e5
irvinzato <multizato@hotmail.com>
2022-07-14 23:54:34 -0500

    Implemente formGroup en mi formulario y cargue selector con hospitales de mi servicio

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Hospital } from './../../../models/hospital.model';
+import { HospitalService } from './../../../services/hospital.service';
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  doctorForm!: FormGroup;
+  hospitals: Hospital [] = [];
+
+  constructor( private fb: FormBuilder, private hospitalService: HospitalService ) { }
 
   ngOnInit(): void {
+    this.doctorForm = this.fb.group({
+      nombre: [ 'Probando', [Validators.required] ],
+      hospital: [ '', [Validators.required] ]
+    });
+
+    this.loadHospitals();
+  }
+
+  loadHospitals() {
+    this.hospitalService.cargarHospitales().subscribe(res => {
+      this.hospitals = res;
+    });
+  }
+
+  saveDoctor() {
+    console.log(this.doctorForm.value);
   }
 
 }
